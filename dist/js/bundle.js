423c5d8eb5959b6960f3ba9244a9738b6f6de067
khaled-web <eng.khaled_ok@outlook.com>
2020-10-12 11:51:46 +0200

    configure the following babel package> core&preset-env&loader&polyfill

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -94,7 +94,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test */ \"./src/js/test.js\");\n// Global app controller\r\n\r\n\r\nconsole.log(`I imported ${_test__WEBPACK_IMPORTED_MODULE_0__[\"default\"]} from another module called test.js!`);\r\n \n\n//# sourceURL=webpack:///./src/js/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test */ \"./src/js/test.js\");\n// Global app controller\r\n\r\nconst x = 23;\r\nconsole.log(`I imported ${_test__WEBPACK_IMPORTED_MODULE_0__[\"default\"]} from another module called test.js! variable x is ${x}`);\r\n \n\n//# sourceURL=webpack:///./src/js/index.js?");
 
 /***/ }),
 
