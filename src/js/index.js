517c9c1a521ddd6688730a8f8216162c2d556b79
khaled-web <eng.khaled_ok@outlook.com>
2020-10-19 17:26:05 +0200

    Building the Search View - Part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,38 @@
-import Search from './Model/search';
-/*
-// TODO: Global state of the app.
-- search object.
-- current recipe object.
-- shopping list object.
-- liked recipes.
+import Search from './Model/Search';
+import * as searchView from './View/searchView';
+import {elements} from './View/base';
 
- */
+/* 
+Global state of app:-
+01. search object
+02. current recipe object
+03. shopping list object
+04. liked recipes
+*/
 
 const state = {};
 
-const controlSearch = async () => {
-  // 01. get the query from the view.
-  const query = 'pasta';
+const controlSearch = async ()=>{
 
-  if (query) {
-    // 02. new search object and add to state.
-    state.search = new Search(query);
+  // 1. get query from view
+  const query = searchView.getInput();
 
-    // 03. prepare UI for result
+  if(query){
+    //2. new search object and add to state
+    state.Search = new Search(query);
+  //3. prepare UI for results
+  searchView.clearInput();
+  searchView.clearResults();
+  //4. search for recipes
+  await state.Search.getResult();
 
-    //04. search for recipes
-    await state.search.getResult()
-
-    // 05. Render results on UI
-     console.log(state.search.result);
+  //5. Render result on UI
+  searchView.renderResult(state.Search.result)
+  
   }
 }
 
-document.querySelector('.search').addEventListener('submit', (e) => {
-  e.preventDefault();
-  controlSearch();
-});
+elements.searchForm.addEventListener('submit', (e)=>{
+e.preventDefault();
+controlSearch();
+})
