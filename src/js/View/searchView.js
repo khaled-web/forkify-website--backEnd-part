252f4ad085541d25217f1ab716c2a25ac18d2eb7
khaled-web <eng.khaled_ok@outlook.com>
2020-10-20 16:38:24 +0200

    17. Building the Search View - extension

--- src/js/View/searchView.js
+++ src/js/View/searchView.js
@@ -1,34 +1,61 @@
-import {elements} from './base';
+import {
+    elements
+} from './base';
 
-export const getInput = ()=> elements.searchInput.value;
+export const getInput = () => elements.searchInput.value;
 
-export const clearInput = ()=>{   
+export const clearInput = () => {
     elements.searchInput.value = '';
 };
 
-export const clearResults = ()=>{
-
-elements.searchResultList.innerHTML = '';
-
+export const clearResults = () => {
+    elements.searchResultList.innerHTML = '';
 };
 
-const renderRecipe = (recipe)=>{
-const markUp = `
+/* 
+// 'pasta with tomato and spinach'
+
+acc: 0 / acc + cur.length: 5 / newTitle: ['pasta']
+acc: 5 / acc + cur.length: 9 / newTitle: ['pasta', 'with']
+acc: 9 / acc + cur.length: 15 / newTitle: ['pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length: 18 / newTitle: ['pasta', 'with', 'tomato']....over the limit.
+
+*/
+
+const limitRecipeTitle = (title, limit = 15) => {
+
+    const newTitle = [];
+
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0)
+        // return to the result
+        return `${newTitle.join(' ')}...`;
+    }
+    return title;
+};
+
+const renderRecipe = (recipe) => {
+    const markUp = `
                  <li>
                     <a class="results__link" href="#${recipe.recipe_id}">
                         <figure class="results__fig">
-                            <img src="${recipe.image_url}" alt="${recipe.title}">
+                            <img src="${recipe.image_url}" alt="${(recipe.title)}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
                 </li>
 `;
-elements.searchResultList.insertAdjacentHTML("beforeend", markUp);
+    elements.searchResultList.insertAdjacentHTML("beforeend", markUp);
 };
 
-export const renderResult = (recipes)=>{
-recipes.forEach(renderRecipe);
+export const renderResult = (recipes) => {
+    recipes.forEach(renderRecipe);
 };
